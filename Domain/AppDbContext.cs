fd6ab01b1a2d7ec3240dc4dd473aa60b61908e25
Taras Dzivik <t.dzivik@gmail.com>
2021-10-28 22:08:48 +0300

    Joined all DbContext classes into the one

--- Domain/AppDbContext.cs
+++ Domain/AppDbContext.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClassJournals.Domain.Entities;
+using ClassJournals.Domain.Entities.CoursesAndGrades;
+using ClassJournals.Domain.Entities.JoiningEntities;
 
 namespace ClassJournals.Domain
 {
@@ -15,21 +17,26 @@ namespace ClassJournals.Domain
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Student> Student { get; set; }
+        public DbSet<Lecture> Lecture { get; set; }
         public DbSet<Lector> Lector { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
-        public DbSet<Lecture> Lecture { get; set; }
+        public DbSet<Grade> Grades { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        public DbSet<StudentLectures> StudentLectures { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
-            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
             {
                 Id = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
                 Name = "admin",
                 NormalizedName = "ADMIN"
             });
 
-            builder.Entity<IdentityUser>().HasData(new IdentityUser
+            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
             {
                 Id = "3b62472e-4f66-49fa-a20f-e7685b9565d8",
                 UserName = "admin",
@@ -42,11 +49,38 @@ namespace ClassJournals.Domain
             });
 
             // Проміжна таблиця в якій ми зв'язуємо айді юзера з його роллю на сайті (адмін)
-            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
             {
                 RoleId = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
                 UserId = "3b62472e-4f66-49fa-a20f-e7685b9565d8"
             });
+
+            modelBuilder.Entity<Student>()
+                .HasOne<Grade>(s => s.Grades)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.CurrentGradeId);
+
+            modelBuilder.Entity<Grade>()
+                .HasMany<Student>(g => g.Students)
+                .WithOne(s => s.Grades)
+                .HasForeignKey(s => s.CurrentGradeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<StudentLectures>().HasKey(sl => new { sl.StudentId, sl.LectureId });
+
+            modelBuilder.Entity<StudentLectures>()
+                .HasOne<Student>(sl => sl.Student)
+                .WithMany(s => s.StudentLectures)
+                .HasForeignKey(sl => sl.StudentId);
+
+            modelBuilder.Entity<StudentLectures>()
+                .HasOne<Lecture>(sl => sl.Lecture)
+                .WithMany(l => l.StudentLectures)
+                .HasForeignKey(sl => sl.LectureId);
+
+
+
+
 
 
         }
