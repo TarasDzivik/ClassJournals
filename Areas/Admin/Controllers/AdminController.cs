db7d08857bb96e33f42c5fada7aeafbf1ff40e1f
Taras Dzivik <t.dzivik@gmail.com>
2021-11-23 19:50:11 +0200

    Other trash of aspnetcore.identity learning process...

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using ClassJournals.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassJournals.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public async Task<IActionResult> GetRolesAdmin()
+        {
+            await roleManager.CreateAsync(new IdentityRole
+            { Name = "Admin", NormalizedName = "ADMIN" });
+            return View(await roleManager.Roles.ToListAsync());
+        }
+        public async Task<IActionResult> GetRolesStudent()
+        {
+            await roleManager.CreateAsync(new IdentityRole
+            { Name = "Student", NormalizedName = "STUDENT" });
+            return View(await roleManager.Roles.ToListAsync());
+        }
+        public async Task<IActionResult> GetRolesLector()
+        {
+            await roleManager.CreateAsync(new IdentityRole
+            { Name = "Lector", NormalizedName = "LECTOR" });
+            return View(await roleManager.Roles.ToListAsync());
+        }
+
+
+    }
+}
