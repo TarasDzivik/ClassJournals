3bb4b7c49000baf5d73f670c86b0baea9b7f2ad9
Taras Dzivik <t.dzivik@gmail.com>
2021-11-24 16:56:43 +0200

    Exception has been thrown by the target of an invocation.

--- Domain/ContextConfigurationsParts/LectorConfiguration.cs
+++ Domain/ContextConfigurationsParts/LectorConfiguration.cs
@@ -1,5 +1,5 @@
-﻿using ClassJournals.Domain.Entities;
-using ClassJournals.Domain.Entities.CoursesAndGroups;
+﻿using ClassJournals.Domain.Entities.Users;
+using ClassJournals.Domain.Entities.Schedules;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,6 +10,17 @@ namespace ClassJournals.Domain.ContextConfigurationsParts
         public void Configure(EntityTypeBuilder<Lector> builder)
         {
             builder.HasKey(l => l.LectorId);
+            // При мыграції викидає помилку:
+            // System.Reflection.TargetInvocationException:
+            // Exception has been thrown by the target of an invocation.
+
+            // --->System.InvalidOperationException: A key cannot be configured
+            // on 'Lector' because it is a derived type.The key must be configured
+            // on the root type 'IdentityUser'.If you did not intend for
+            // 'IdentityUser' to be included in the model, ensure that it
+            // is not included in a DbSet property on your context, referenced
+            // in a configuration call to ModelBuilder, or referenced from
+            // a navigation property on a type that is included in the model.
 
             builder.Property(s => s.FirstName).IsRequired()
                 .HasMaxLength(15).HasColumnType("varchar")
